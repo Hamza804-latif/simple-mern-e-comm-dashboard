1e8d8dbd66a060ac7277a9ef7bbb4d892394c904
Hamza804-latif <90188142+Hamza804-latif@users.noreply.github.com>
2023-01-04 12:51:41 +0500

    front end design and database connection

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,21 @@
 const express = require("express");
+const cors = require("cors");
+require("./db/config");
+const User = require("./db/User");
 
 const app = express();
-app.get("/", (req, resp) => {
-  resp.send("this is home route");
+app.use(express.json());
+app.use(cors());
+app.post("/register", (req, resp) => {
+  const user = new User(req.body);
+  user.save((err, result) => {
+    if (err) {
+      resp.send(err);
+    } else {
+      resp.send(result);
+      console.log("response", result);
+    }
+  });
 });
 
 app.listen(5000);
